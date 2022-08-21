94aa1d462a9a00c0b9ec8f6e208b689b9f8db82b
zmhp <manashahp1995@gmail.com>
2022-08-21 20:14:13 +0530

    Static and Dynamic Variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+
+	let title = 'Welcome to the Blog';
+	let likes = 50;
+	let link = 'http://www.orendax.com';
+
 	return (
 		<div className="App">
 			<div className="content">
-				<h1>App Component</h1>
+				<h1>{ title }</h1>
+				<p>Liked { likes } times</p>
+
+				<p>{ 10 }</p>
+				<p>{ 'STRING' }</p>
+				<p>{ [1,2,3,4,5] }</p>
+				<p>{ Math.random() * 20 }</p>			
+				<a href={ link }>OrendaX</a>
+
 			</div>
 		</div>
 	);
