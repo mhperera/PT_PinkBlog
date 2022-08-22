e26af8f848e81983f9d2516faf02874cfeb734eb
zmhp <manashahp1995@gmail.com>
2022-08-22 12:21:31 +0530

    Remove ClickAgain Button

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,7 @@
 const Home = () => {
 
-    const name = 'Mario';
-
     const handleClick = (e) => {
-        console.log("Hello All", e);
-    }
-
-    const handleClickAgain = (name,e) => {
-        console.log("Hello " + name, e.target);
+        
     }
 
     return (
@@ -17,10 +11,6 @@ const Home = () => {
             <h2>Home Page</h2>
 
             <button onClick={ handleClick }>Click Me</button>
-
-            <button onClick={ (e) => {
-                handleClickAgain(name,e);
-            } }>Click Me Again</button>
 
         </div>
 
