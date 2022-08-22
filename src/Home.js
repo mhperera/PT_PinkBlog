adbb5cebaca1dd91dd1730cc4b375e052666975c
zmhp <manashahp1995@gmail.com>
2022-08-22 12:17:52 +0530

    Button Click Event

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,27 @@
 const Home = () => {
+
+    const name = 'Mario';
+
+    const handleClick = (e) => {
+        console.log("Hello All", e);
+    }
+
+    const handleClickAgain = (name,e) => {
+        console.log("Hello " + name, e.target);
+    }
+
     return (
 
         <div className="home">
+            
             <h2>Home Page</h2>
+
+            <button onClick={ handleClick }>Click Me</button>
+
+            <button onClick={ (e) => {
+                handleClickAgain(name,e);
+            } }>Click Me Again</button>
+
         </div>
 
     );
