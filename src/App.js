3f86f031e824d7c4b44a6c4c413cf36b52f4375b
zmhp <manashahp1995@gmail.com>
2022-08-22 10:38:11 +0530

    Add Styles for Navbar and Home Components Globally

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-
-	let title = 'Welcome to the Blog';
-	let likes = 50;
-	let link = 'http://www.orendax.com';
 
 	return (
 
