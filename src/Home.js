7a1c6de8a46a72be9db3e91aea7f93a9e4d29ee6
zmhp <manashahp1995@gmail.com>
2022-08-22 15:01:36 +0530

    use useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './Bloglist';
 
 const Home = () => {
@@ -28,6 +28,10 @@ const Home = () => {
         const newBlogs = blogs.filter( blog =>  blog.id != id );
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        
+    });
 
     return (
 
