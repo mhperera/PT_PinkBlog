7eb1943bb6c048cc93354b743091594bb8ef5d07
zmhp <manashahp1995@gmail.com>
2022-08-22 16:44:31 +0530

    Fetch Data from db.json

--- src/Home.js
+++ src/Home.js
@@ -3,41 +3,28 @@ import BlogList from './Bloglist';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {
-            id: 1,
-            title : 'My new website',
-            body : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries...',
-            author: 'S Mario'
-        },
-        {
-            id: 2,
-            title : 'Welcome Party',
-            body : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries...',
-            author: 'Bobby Diol'
-        },
-        {
-            id: 3,
-            title : 'Web Dev tool tips',
-            body : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries...',
-            author: 'S Mario'
-        }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter( blog =>  blog.id != id );
+        const newBlogs = blogs.filter( blog =>  blog.id !== id );
         setBlogs(newBlogs);
     }
 
     useEffect(() => {
-
+        fetch('http://localhost:8000/blogs') // promise
+        .then(res => {
+            return res.json();
+        })
+        .then((data)=>{
+            setBlogs(data);
+        })
     }, []);
 
     return (
 
         <div className="home">
             
-            <BlogList blogs={ blogs } title='All Blogs' handleDelete={ handleDelete }/>
+            {blogs && <BlogList blogs={ blogs } title='All Blogs' handleDelete={ handleDelete }/>}
 
             {/* <BlogList blogs={ blogs.filter((blog) => ( blog.author === 'S Mario' ) ) } title='Marios Blogs' handleDelete={ handleDelete }/> */}
 
