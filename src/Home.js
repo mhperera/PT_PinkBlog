e555bc4a0ec9ed44fecc7889e7d11a8652ecc953
zmhp <manashahp1995@gmail.com>
2022-08-22 12:41:43 +0530

    use useState hook to make reactive variables and change the value and render the new value

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 const Home = () => {
 
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(25);
+
     const handleClick = (e) => {
-        
+        setName('Luigi');
+        setAge(35);
     }
 
     return (
@@ -10,11 +16,14 @@ const Home = () => {
             
             <h2>Home Page</h2>
 
+            <p>{ name } is { age } years old.</p>
+
             <button onClick={ handleClick }>Click Me</button>
 
         </div>
 
     );
+
 }
  
 export default Home;
