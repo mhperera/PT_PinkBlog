09c62cebff9bd76b531458e6d3c5372bab14732c
zmhp <manashahp1995@gmail.com>
2022-08-22 09:53:21 +0530

    Multiple Components (Home/Navbar)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
@@ -7,19 +9,17 @@ function App() {
 	let link = 'http://www.orendax.com';
 
 	return (
+
 		<div className="App">
+
+			<Navbar />
+
 			<div className="content">
-				<h1>{ title }</h1>
-				<p>Liked { likes } times</p>
-
-				<p>{ 10 }</p>
-				<p>{ 'STRING' }</p>
-				<p>{ [1,2,3,4,5] }</p>
-				<p>{ Math.random() * 20 }</p>			
-				<a href={ link }>OrendaX</a>
-
+				<Home />
 			</div>
+			
 		</div>
+
 	);
 }
 
