142ca83fd9cd8a1f3a111e524d65de13f82d00ac
zmhp <manashahp1995@gmail.com>
2022-08-22 13:49:33 +0530

    Reuse BlogList component and use filter function

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,8 +1,5 @@
 const BlogList = ({ blogs, title }) => {
 
-    // const blogs = props.blogs;
-    // const title = props.title;
-
     return ( 
 
         <div className="blog-list">
--- src/Home.js
+++ src/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
             id: 3,
             title : 'Web Dev tool tips',
             body : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries...',
-            author: 'Tiger Sheroff'
+            author: 'S Mario'
         }
     ])
 
@@ -29,6 +29,10 @@ const Home = () => {
         <div className="home">
             
             <BlogList blogs={ blogs } title='All Blogs'/>
+
+            <br />
+                
+            <BlogList blogs={ blogs.filter((blog) => ( blog.author === 'S Mario' ) ) } title='Marios Blogs'/>
 
         </div>
 
