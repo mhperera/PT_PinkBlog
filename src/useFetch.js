d26f72043f693380b5ffc6c0c778ee98df941611
zmhp <manashahp1995@gmail.com>
2022-08-23 13:47:50 +0530

    cleanup function and abortController

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,40 +2,62 @@ import { useState, useEffect } from 'react';
 
 // CUSTOM HOOK
 const useFetch = (url) => {
-
+    
     const [data, setData] = useState(null);
+
     const [isPending, setIsPending] = useState(true);
+
     const [error, setError] = useState(null); 
 
     useEffect(() => {
-        
-        // setTimeout(()=>{
 
-            fetch(url) // promise
+        const abortCont = new AbortController();
+
+        setTimeout(()=>{
+
+            fetch(url, { signal:abortCont.signal } ) // promise
 
             .then(res => {
 
                 // CHECK ERRORS COMING BACK FROM THE SERVER (This also catchs from the .catch() block) (To check change the endpoint)
                 if(!res.ok){
+
                     throw Error('Could not fetch data for that resource.');
+                    
                 }
 
                 return res.json();
             })
 
             .then((data)=>{
+
                 setData(data);
+
                 setIsPending(false);
+
                 setError(null);
+
             })
 
             // CHECK FOR THE NETWORK ERROR (To check this abort json server)
             .catch((err)=>{
-                setIsPending(false);
-                setError(err.message);
+                
+                if(err.name === 'AbortError'){
+
+                    console.log('aborted');
+
+                }else{
+
+                    setIsPending(false);
+                    setError(err.message);
+
+                }
+                
             })
 
-        // },1000)
+        },1000)
+
+        return () => abortCont.abort();
 
     }, [url]);
 
