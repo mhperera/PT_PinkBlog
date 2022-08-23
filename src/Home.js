987200143720bf7a65cef2ac32b61b2a787712a8
zmhp <manashahp1995@gmail.com>
2022-08-23 10:39:25 +0530

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,44 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null); 
 
     useEffect(() => {
         setTimeout(()=>{
+
             fetch('http://localhost:8000/blogs') // promise
+
             .then(res => {
+
+                // CHECK ERRORS COMING BACK FROM THE SERVER (This also catchs from the .catch() block) (To check change the endpoint)
+                if(!res.ok){
+                    throw Error('Could not fetch data for that resource.');
+                }
+
                 return res.json();
             })
+
             .then((data)=>{
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
             })
+
+            // CHECK FOR THE NETWORK ERROR (To check this abort json server)
+            .catch((err)=>{
+                setIsPending(false);
+                setError(err.message);
+            })
+
         },1000)
+
     }, []);
 
     return (
 
         <div className="home">
+
+            { error && <div>{ error }</div> }
 
             { isPending && <div>Loading...</div> }
 
