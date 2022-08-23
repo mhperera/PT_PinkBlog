6f6ca641fb2297eb4e8720c6580e57a7461ae22e
zmhp <manashahp1995@gmail.com>
2022-08-23 22:31:39 +0530

    Add controlled inputs for the create blog form

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,34 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
+    const { data:blog , error, isPending } = useFetch(`http://localhost:8000/blogs/` + id );
 
     return ( 
         <div className="blog-details">
-            <h2>Blog Details- { id }</h2>
+
+            { isPending && <div>Loading...</div> }
+            
+            { error && <div>{ error }</div> }
+
+            { blog && (
+                <article>
+
+                    <h2>{ blog.title }</h2>
+
+                    <br />
+
+                    <p>Written By : { blog.author } </p>
+
+                    <br />
+                    
+                    <p> { blog.body } </p>
+
+                </article>
+            ) }
+
         </div>
     );
 }
