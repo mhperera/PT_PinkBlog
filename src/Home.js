d8c626eb12af9443cf23f629a2148c161149aff6
zmhp <manashahp1995@gmail.com>
2022-08-23 10:19:11 +0530

    Set Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,27 @@ import BlogList from './Bloglist';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs') // promise
-        .then(res => {
-            return res.json();
-        })
-        .then((data)=>{
-            setBlogs(data);
-        })
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs') // promise
+            .then(res => {
+                return res.json();
+            })
+            .then((data)=>{
+                setBlogs(data);
+                setIsPending(false);
+            })
+        },1000)
     }, []);
 
     return (
 
         <div className="home">
-            
+
+            { isPending && <div>Loading...</div> }
+
             {blogs && <BlogList blogs={ blogs } title='All Blogs' />}
 
             {/* <BlogList blogs={ blogs.filter((blog) => ( blog.author === 'S Mario' ) ) } title='Marios Blogs' handleDelete={ handleDelete }/> */}
